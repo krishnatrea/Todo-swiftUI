159fc394d9ec428f90779cc73dbca384e1aa85d6
krishnatrea <krishnatrea@gmail.com>
2023-09-15 00:33:18 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Krishnatrea on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
